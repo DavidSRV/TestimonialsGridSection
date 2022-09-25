78f24cab5c7c18002c29c4b4982682b29467edbf
David Ruiz <david.ruizvargas@hotmail.com>
2022-09-24 21:50:36 -0500

    First Commit

---
+++ src/utils/img.js
@@ -0,0 +1,13 @@
+import {daniel} from '../assets/images/image-daniel.jpg'
+import {jeannette} from '../assets/images/image-jeanette.jpg'
+import {jonathan} from '../assets/images/image-jonathan.jpg'
+import {kira} from '../assets/images/image-kira.jpg'
+import {patrick} from '../assets/images/image-patrick.jpg'
+
+export const objImg = {
+    daniel,
+    jeannette,
+    jonathan,
+    kira,
+    patrick
+}
